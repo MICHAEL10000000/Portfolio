4d9b241be5476c31eab2dcc37287f610b224d2ae
Gbenga <divadaforbusiness@gmail.com>
2024-05-02 06:16:53 +0100

    latest update

--- src/app.js
+++ src/app.js
@@ -16,3 +16,28 @@ allList.forEach(li=> {
     })
 })
 console.log("HI")
+
+document.getElementById("contactform").addEventListener("submit", function(event) {
+    // Prevent default form submission
+    event.preventDefault();
+
+    // Send the form using AJAX
+    var form = event.target;
+    var formData = new FormData(form);
+    console.log(formData)
+    var xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+            // Success message
+            alert("Thank you! Your message has been sent.");
+            form.reset(); // Reset the form
+        } else {
+            // Error message
+            alert("Oops! Something went wrong.");
+        }
+    };
+    xhr.send(formData);
+});
